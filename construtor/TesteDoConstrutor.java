a4e91120e034f5ad5081552c47e1e384d4c3818a
Afranio <afraniodantas0224@gmail.com>
2024-12-29 17:21:05 -0300

    feat: constructor

---
+++ construtor/TesteDoConstrutor.java
@@ -0,0 +1,12 @@
+package construtor;
+
+public class TesteDoConstrutor {
+
+    public static void mian(String[] args) {
+
+        Construtor construtor = new Construtor(2, "string");
+        System.out.println(construtor);
+
+    }
+    
+}
